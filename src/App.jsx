0f7166a27641b3866e852050ad6f9472ec2a9154
Diego Tellez <diegotellezcontreras@gmail.com>
2023-04-01 23:13:07 -0500

    Agregados los fondos y el objeto bgImages

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,14 @@ function App() {
     setCoords(currentCoords)
   }
 
+  console.log(weather)
+
+  // Efecto para la localización
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success)
   }, [])
 
+  // Efecto del llamado a la API
   useEffect(() => {
     if(coords) {
       const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=68187a38d653607d35b491707478c55a`
@@ -45,10 +49,34 @@ function App() {
     }
   }, [coords])
   
+  const bgImages = {
+    "01d": '/images/bgImages/bg01n.jpeg',
+    "01n": '/images/bgImages/bg01n.jpeg',
+    "02d": '/images/bgImages/bg02d.jpeg',
+    "02n": '/images/bgImages/bg02n.jpeg',
+    "03d": '/images/bgImages/bg03d.jpeg',
+    "03n": '/images/bgImages/bg03n.jpeg',
+    "04d": '/images/bgImages/bg04d.jpeg',
+    "04n": '/images/bgImages/bg04n.jpeg',
+    "09d": '/images/bgImages/bg09d.jpeg',
+    "09n": '/images/bgImages/bg09n.jpeg',
+    "10d": '/images/bgImages/bg10d.jpeg',
+    "10n": '/images/bgImages/bg10n.jpeg',
+    "11d": '/images/bgImages/bg11d.jpeg',
+    "11n": '/images/bgImages/bg11n.jpeg',
+    "13d": '/images/bgImages/bg13d.jpeg',
+    "13n": '/images/bgImages/bg13n.jpeg',
+    "50d": '/images/bgImages/bg50d.jpeg',
+    "50n": '/images/bgImages/bg50n.jpeg',
+
+  }
+
+  const prueba = '/images/bgImages/bg50n.jpeg'
+  const prueba2 = '/images/bgImages/bg10n.jpeg'
 
 
   return (
-    <div className="App bg-[url('/images/weatherGeneric.jpg')] bg-cover min-h-screen grid place-content-center px-2">
+    <div className={`App bg-[url('${bgImages["50n"]}')] bg-cover min-h-screen grid place-content-center px-2`}>
       
       {
         weather ? (
