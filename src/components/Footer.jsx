5cb736832c6e6d4804e0193d99cbb10fadd946a0
Diego Tellez <diegotellezcontreras@gmail.com>
2023-05-15 20:19:26 -0500

    Mejorados algunos estilos en el loader y el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,25 +5,25 @@ const Footer = () => {
         <footer className='mt-auto flex flex-col items-center text-white mb-6 gap-2 md:flex-row md:justify-evenly'>
             
             <div className='flex gap-x-4 md:order-last'>
-                <a className='text-3xl text-secondary-color  hover:animate-bounce' target='_blank' href="https://github.com/diegotellezc">
+                <a className='text-3xl hover:animate-bounce' target='_blank' href="https://github.com/diegotellezc">
                     <i className='bx bxl-github' ></i>
                 </a>
 
-                <a className='text-3xl text-secondary-color  hover:animate-bounce' target='_blank' href="https://www.linkedin.com/in/diegotellezc/">
+                <a className='text-3xl hover:animate-bounce' target='_blank' href="https://www.linkedin.com/in/diegotellezc/">
                     <i className='bx bxl-linkedin-square' ></i>
                 </a>
 
-                <a className='text-3xl text-secondary-color  hover:animate-bounce' target='_blank' href="https://diegotellez-portfolio.netlify.app/">
+                <a className='text-3xl hover:animate-bounce' target='_blank' href="https://diegotellez-portfolio.netlify.app/">
                     <i class='bx bxs-user-circle'></i>
                 </a>
 
-                <a className='text-3xl text-secondary-color  hover:animate-bounce' target='_blank' href="https://easyshop-bydt.netlify.app/">
+                <a className='text-3xl hover:animate-bounce' target='_blank' href="https://easyshop-bydt.netlify.app/">
                     <i className='bx bx-question-mark' ></i>
                 </a>
             </div>
 
-            <p className='text-xs sm:text-base md:order-2 text-secondary-color'>• Copyright ©2023 | All rights reserved •</p>
-            <p className='text-xs sm:text-base text-secondary-color'>@diegotellezc</p>
+            <p className='text-xs sm:text-base md:order-2'>• Copyright ©2023 | All rights reserved •</p>
+            <p className='text-xs sm:text-base'>@diegotellezc</p>
         </footer>
     )
 }
